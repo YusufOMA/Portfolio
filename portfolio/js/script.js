424f435446d1d55e8cb544eea177e40bd46775a3
YusufOMA <90691334+YusufOMA@users.noreply.github.com>
2022-05-28 14:55:21 +0200

    Filter toegevoegd

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,7 +1,5 @@
 
 
-
-
 const menu = document.querySelector('.nav_menu');
 const menuBtn = document.querySelector('#open-menu-btn');
 const closeBtn = document.querySelector('#close-menu-btn');
@@ -24,3 +22,101 @@ closeBtn.addEventListener('click', closeNav);
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY>100);
 })
+
+
+
+
+//filter
+// alle projecten opgehaald met de klasse project
+let allprojects = document.getElementsByClassName("project");
+// alle elementen met de klasse filter opgehaald
+let filters = document.getElementsByClassName("filter");
+
+window.scrollTo(0,0)
+setInterval(function(){
+    document.getElementsByTagName("body")[0].style.overflow = "auto";
+}, 1500);
+
+for(let i = 0; i < filters.length; i++){
+    filters[i].checked = true;
+}
+
+//sport
+let vrFilter = document.getElementById("checkbox-vr");
+vrFilter.onchange = function () {
+    //hij gaat checken of het aan of uit staat
+    if (vrFilter.checked === true) {
+        for (let i = 0; i < allprojects.length; i++) {
+            if (allprojects[i].dataset.category === "vr") {
+                allprojects[i].style.display = "block";
+            }
+        }
+    }
+    else {
+        //hier loop je door alle projecten met een for loop
+        for (let i = 0; i < allprojects.length; i++) {
+            if (allprojects[i].dataset.category === "vr") {
+                allprojects[i].style.display = "none";
+            }
+        }
+    }
+}
+
+// RPG
+let apiFilter = document.getElementById("checkbox-api");
+apiFilter.onchange = function () {
+    if (apiFilter.checked === true) {
+        for (let i = 0; i < allprojects.length; i++) {
+            if (allprojects[i].dataset.category === "api") {
+                allprojects[i].style.display = "block";
+            }
+        }
+    }
+    else {
+        for (let i = 0; i < allprojects.length; i++) {
+            if (allprojects[i].dataset.category === "api") {
+                allprojects[i].style.display = "none";
+            }
+        }
+    }
+}
+
+
+// style
+let styleFilter = document.getElementById("checkbox-style");
+styleFilter.onchange = function () {
+    if (styleFilter.checked === true) {
+        for (let i = 0; i < allprojects.length; i++) {
+            if (allprojects[i].dataset.category === "style") {
+                allprojects[i].style.display = "block";
+            }
+        }
+    }
+    else {
+        for (let i = 0; i < allprojects.length; i++) {
+            if (allprojects[i].dataset.category === "style") {
+                allprojects[i].style.display = "none";
+            }
+        }
+    }
+}
+
+let sitesFilter = document.getElementById("checkbox-sites");
+sitesFilter.onchange = function () {
+    if (sitesFilter.checked === true) {
+        for (let i = 0; i < allprojects.length; i++) {
+            if (allprojects[i].dataset.category === "sites") {
+                allprojects[i].style.display = "block";
+            }
+        }
+    }
+    else {
+        for (let i = 0; i < allprojects.length; i++) {
+            if (allprojects[i].dataset.category === "sites") {
+                allprojects[i].style.display = "none";
+            }
+        }
+    }
+}
+
+
